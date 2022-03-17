2fc942f01aec9963b304a8449fdac381d143b237
mingming zhong <mingming.zhong@shand118.sprd.com>
2022-03-17 02:19:23 +0000

    Bug #1846416 A13 add gnss xml,cert and mk
    [root cause  ] A13 bringup
    [changes     ] vendor/sprd/modules/wcn/vendor/gnss
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:ums512
    [test case]:gnss probe
    [reviewers   ] Andy Huang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Thia patch is used to add gnss xml,cert and mk.
    
    Change-Id: I02cc3f5e3697180c796ebee3b77f0a26298b2bcc
    Signed-off-by: mingming zhong <mingming.zhong@shand118.sprd.com>

---
+++ gnss/device-sprd-gps.mk
@@ -0,0 +1,15 @@
+ifeq ($(strip $(SUPPORT_GNSS_HARDWARE)), true)
+
+$(warning shell echo "device-sprd-gps_mk_entry")
+PRODUCT_COPY_FILES += \
+	vendor/sprd/modules/wcn/vendor/gnss/misc/spirentroot.cer:/vendor/etc/spirentroot.cer \
+	vendor/sprd/modules/wcn/vendor/gnss/misc/supl.xml:/vendor/etc/supl.xml \
+	vendor/sprd/modules/wcn/vendor/gnss/misc/config.xml:/vendor/etc/config.xml
+$(warning shell echo "device-sprd-gps_mk_entry1")
+PRODUCT_PACKAGES += \
+    gpsd \
+
+$(warning shell echo "device-sprd-gps_mk_end")
+endif
+
+
