2e9672c2c3d8b44525f6155aea03e2066dd58abd
qingsong.zhang <qingsong.zhang@unisoc.com>
2023-04-24 14:10:45 +0800

    Bug #2230670 a14 code解耦 FM 删除“/vendor/media/engtest_sample.pcm"
    [root cause  ]:the file is removed because a14 follower
    [changes     ]:fm libbat
    [side effects]:n/a
    [self test   ]:pass
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:9863
    [test case]:pass
    [reviewers]:xiaopeng.gao,lun.zhao
    
    commit_template_version:v1
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I0f746756db886dc13e4ad5cadf28873e3d5ce509

--- fm/libbbat/audiocomm.h
+++ fm/libbbat/audiocomm.h
@@ -3,7 +3,7 @@
 
 #define AUDCTL "/dev/pipe/mmi.audio.ctrl"
 #define AUDIO_EXT_DATA_CONTROL_PIPE "/data/local/media/mmi.audio.ctrl"
-#define SPRD_AUDIO_FILE "/vendor/media/engtest_sample.pcm"
+//#define SPRD_AUDIO_FILE "/vendor/media/engtest_sample.pcm" bug 2230670
 
 int SendAudioTestCmd(const uchar * cmd,int bytes);
 
