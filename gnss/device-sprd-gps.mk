c794b96e200554043cd72217d767f5b96760cfee
liling <ling1.li@chino-e.com>
2023-11-28 16:27:55 +0800

    [moto_g35-286][GPS][liling]:Configure SUPL(1)
    Description:
    Configure supl cer, host and port according to mnc/mcc (unisoc patch).
    
    Change-Id: I365917f2f9b7be33f8f2c3f3e8e425388650f7c0

--- gnss/device-sprd-gps.mk
+++ gnss/device-sprd-gps.mk
@@ -5,6 +5,9 @@ PRODUCT_COPY_FILES += \
 	vendor/sprd/modules/wcn/vendor/gnss/misc/spirentroot.cer:/vendor/etc/spirentroot.cer \
 	vendor/sprd/modules/wcn/vendor/gnss/misc/supl.xml:/vendor/etc/supl.xml \
 	vendor/sprd/modules/wcn/vendor/gnss/misc/config.xml:/vendor/etc/config.xml
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/sprd/modules/wcn/vendor/gnss/misc/supl,$(TARGET_COPY_OUT_VENDOR)/etc/supl)
 $(warning shell echo "device-sprd-gps_mk_entry1")
 
 ifneq (,$(filter $(strip $(SPRD_MODULES_NAVC_PATH)),lite lite-Integ))
