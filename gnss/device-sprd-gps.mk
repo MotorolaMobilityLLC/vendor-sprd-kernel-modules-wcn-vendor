4fe86993697aa819de9befe892176a8f94fe4dfc
zhouquan <zhouquan@longcheer.com>
2024-07-31 16:57:55 +0800

    [issue][GPS][EKissue-11397]UNISOC TEST PATCH
    [rootcause]:AGPS function PATCH
    [solution]:AGPS function PATCH
    [sideeffect]: None
    [other]: None
    
    Change-Id: I64cf55610ee99f9003ac39738b93f7974e3fc9a8

--- gnss/device-sprd-gps.mk
+++ gnss/device-sprd-gps.mk
@@ -2,7 +2,9 @@ ifeq ($(strip $(SUPPORT_GNSS_HARDWARE)), true)
 
 $(warning shell echo "device-sprd-gps_mk_entry")
 PRODUCT_COPY_FILES += \
-	vendor/sprd/modules/wcn/vendor/gnss/misc/spirentroot.cer:/vendor/etc/spirentroot.cer \
+	vendor/sprd/modules/wcn/vendor/gnss/misc/suplrootca.pem:/vendor/etc/suplrootca.pem \
+	vendor/sprd/modules/wcn/vendor/gnss/misc/client.crt:/vendor/etc/client.crt \
+	vendor/sprd/modules/wcn/vendor/gnss/misc/client_key.pem:/vendor/etc/client_key.pem \
 	vendor/sprd/modules/wcn/vendor/gnss/misc/supl.xml:/vendor/etc/supl.xml \
 	vendor/sprd/modules/wcn/vendor/gnss/misc/config.xml:/vendor/etc/config.xml
 $(warning shell echo "device-sprd-gps_mk_entry1")
